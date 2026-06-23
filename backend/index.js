1e3b6c5115d540e8ab0f5dc8ad7b1b92c3b8969a
uttamvajapara-a16y <uttamvajapara@gmail.com>
2026-06-23 13:59:15 +0530

    cloudinary and product routes

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,18 @@ connectDB() ;
 
 const app = express() ;
 app.use(cors()) ;
+app.use(express.json()) ;
+app.use(express.urlencoded({extended:true})) ;
 
 app.get('/',(req,res)=>{
     res.json({message:'hello from backend'}) ;
 }) ;
 
 app.use('api/auth' , require('./routes/authRoutes')) ;
+app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/payment', require('./routes/paymentRoutes'));
+app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT, ()=> console.log(`server is running on port ${PORT}`)) ;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,11 @@
-const express = require('express') ;
-const router = express.Router() ;
-const { registerUser, loginUser, getUsers } = require('../controllers/authController') ;
+const express = require('express');
+const { registerUser, loginUser, getUsers } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
+const { admin } = require('../middleware/adminMiddleware');
+const router = express.Router();
 
-router.post('/register', registerUser) ;
-router.post('/login', loginUser) ;
-router.post('/user' , getUsers) ;
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/users', protect, admin, getUsers);
+
+module.exports = router;
